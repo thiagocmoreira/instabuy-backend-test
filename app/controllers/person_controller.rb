0d3d479687f584aabaf0e581f77bd255454d4343
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-24 02:51:22 -0300

    :art: Adding methos put and delete to person controller

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -11,7 +11,7 @@ class PersonController < ApplicationController
   # GET - List one Person
   def show
     @person = Person.find(params[:id])
-    
+
     respond_to do |format|
       format.json { render :json => @person }
     end
@@ -22,15 +22,46 @@ class PersonController < ApplicationController
     @person = Person.new(person_params)
 
     if @person.save
-      render json: { status: 'Success', id: @person.id }, status: :ok
+      render json: { status: 'POST Success', id: @person.id }, status: :ok
     else
-      render json: { status: 'Error', message:'Person not saved!', data: @person.errors }, status: :unprocessable_entity
+      render json: { status: 'Error', message:'Error registering a new person', person: @person.errors }, status: :unprocessable_entity
     end
   end
 
+  # PUT - Update one Person using the body
+  def update_by_body
+    @person = Person.find(person_update_params[:id])
+
+    if @person.update_attributes(person_update_params)
+      render json: { status: 'PUT Success' }, status: :ok
+    else
+      render json: { status: 'Error', message:'Error updating person', person: @person.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE - Destroy one Person register using the body
+  def destroy_by_body
+    @person = Person.find(person_delete_param[:id])
+    id = @person.id
+
+    if @person.destroy
+      render json: { status: 'DELETE Success', id: id }, status: :ok
+    else
+      render json: { status: 'Error', message:'Error deleting person', person: @person.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # Encapsulated params
   private
     def person_params
-      params.require(:person).permit(:name, :age, :cpf)
+      params.permit(:name, :age, :cpf)
     end
 
+    def person_update_params
+      params.permit(:id, :age)
+    end
+
+    def person_delete_param
+      params.permit(:id)
+    end
 end
