af9b29d173ed6124525efe47ef75bfc790692c58
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-24 15:58:55 -0300

    :art: Changing address routes to correct spelling

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   # PUT using the body
   put '/person' => 'person#update_by_body'
-  put '/adress' => 'adress#update_by_body'
+  put '/address' => 'adress#update_by_body'
   # DELETE using the body
   delete '/person' => 'person#destroy_by_body'
-  delete '/adress' => 'adress#destroy_by_body'
+  delete '/address' => 'adress#destroy_by_body'
   # Use routes mapped on the CORS, of application.rb (get, put, delete, post)
   resources :person, :defaults => { :format => :json } do
     # PUT using the query
-    get '/adress' => 'adress#show_by_body'
+    get '/address' => 'adress#show_by_body'
   end
-  resources :adress, :defaults => { :format => :json }
+  # Address routes
+  get '/address' => 'adress#index', :defaults => { :format => :json }
+  get '/address/:id' => 'adress#show', :defaults => { :format => :json }
+  post '/address/' => 'adress#create', :defaults => { :format => :json }
+  delete '/address/:id' => 'adress#destroy', :defaults => { :format => :json }
 end
