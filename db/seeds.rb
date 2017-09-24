72555ae5f306d682a8d00f9ad79a3a5a066ce903
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-24 04:13:57 -0300

    :art: Adding seeds to populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Create 25 People to populate db
 25.times do
 	Person.create({
-		name: Faker::Pokemon.name,
+		name: Faker::GameOfThrones.character,
     age: Faker::Number.number(2),
     cpf: Faker::Number.number(11)
+	})
+end
+
+# Create 24 Address to populate db
+8.times do
+	Adress.create({
+		zipcode: Faker::Number.number(8),
+    state: Faker::Address.state,
+    city: Faker::GameOfThrones.city,
+    street: Faker::Address.street_name,
+    number: Faker::Address.building_number,
+    person_id: 1
+	})
+end
+
+8.times do
+	Adress.create({
+		zipcode: Faker::Number.number(8),
+    state: Faker::Address.state,
+    city: Faker::GameOfThrones.city,
+    street: Faker::Address.street_name,
+    number: Faker::Address.building_number,
+    person_id: 2
+	})
+end
+
+8.times do
+	Adress.create({
+		zipcode: Faker::Number.number(8),
+    state: Faker::Address.state,
+    city: Faker::GameOfThrones.city,
+    street: Faker::Address.street_name,
+    number: Faker::Address.building_number,
+    person_id: 3
 	})
 end
